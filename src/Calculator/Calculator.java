2a5e7fd0b5b2b391422b34b308c48314071ba1f7
HyperEr00 <kostantinos.mel@gmail.com>
2022-01-29 16:55:11 +0200

    text allign to right and changed color

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package codejavaapp;
+package Calculator;
 
 /**
  *
  * @author Hyperer
  */
-public class CodeJavaApp {
+public class Calculator {
 
     /**
      * @param args the command line arguments
--- src/Calculator/CalculatorFrame.java
+++ src/Calculator/CalculatorFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package codejavaapp;
+package Calculator;
 
 import java.awt.ComponentOrientation;
 import java.awt.TextArea;
